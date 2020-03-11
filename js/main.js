01efa4b78439e519dcb55e59750f38512a70b1cc
dazzilion <dazzilion@github.com>
2020-03-11 20:53:31 +0200

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,9 @@
+$('.tributary').hover (
+	function() {
+		$('.description').html($(this).attr('description-data'));
+		$('.description').fadeIn();
+	},
+	function() {
+		$('.description').fadeOut(50);
+	}
+)
